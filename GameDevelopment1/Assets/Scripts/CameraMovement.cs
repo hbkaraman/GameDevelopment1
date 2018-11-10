98d7453b5f18f9dba64f0fa9afc8952ad6652ab9
brtyldz1 <brtyldz32@gmail.com>
2018-11-10 23:46:33 +0300

    CameraMovement
    CameraMovement eklendi, PlayerMovement koduyla birleştirildi.

---
+++ GameDevelopment1/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Camera gameplayCamera;
+    //float shakeAmount = 0f;
+    public float speed;
+
+    public Transform room1;
+    public Transform room2;
+    public Transform room3;
+    public Transform room4;
+
+    public PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        if (gameplayCamera == null)
+        {
+            gameplayCamera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    /*#region SHAKE
+    public void Shake(float amount, float duration)
+    {
+        shakeAmount = amount;
+        InvokeRepeating("BeginShake", 0, 0.01f);
+        Invoke("StopShake", duration);
+    }
+
+    void BeginShake()
+    {
+        if (shakeAmount > 0)
+        {
+            Vector2 camPos = gameplayCamera.transform.position;
+
+            float offsetX = Random.value * shakeAmount * 2 - shakeAmount;
+            float offsetY = Random.value * shakeAmount * 2 - shakeAmount;
+            camPos.x += offsetX;
+            camPos.y += offsetY;
+            gameplayCamera.transform.position = camPos;
+
+        }
+
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("BeginShake");
+        gameplayCamera.transform.localPosition = Vector2.zero;
+    }
+    #endregion*/
+
+    public void CameraShift()
+    {
+        if (playerMovement.roomCount == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, room1.position, speed * Time.deltaTime);
+        }
+        else if (playerMovement.roomCount == 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, room2.position, speed * Time.deltaTime);
+        }
+        else if (playerMovement.roomCount == 3)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, room3.position, speed * Time.deltaTime);
+        }
+        else if (playerMovement.roomCount == 4)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, room4.position, speed * Time.deltaTime);
+        }
+    }
+}
--- GameDevelopment1/Assets/Scripts/PlayerMovement.cs
+++ GameDevelopment1/Assets/Scripts/PlayerMovement.cs
@@ -2,74 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	public Rigidbody2D rb;
-	private Vector2 localScale;
-	private Vector2 moveVelocity;
+    public Rigidbody2D rb;
+    private Vector2 localScale;
+    private Vector2 moveVelocity;
 
-	public bool isMoving =false;
-	public float speed;
+    public bool isMoving = false;
+    public float speed;
 
-	public BulletScript bulletSc;
-	public GameObject bullet;
+    public BulletScript bulletSc;
+    public GameObject bullet;
 
-	private float timeBtwShots;
-	public float startTimeBtwShots;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public int roomCount;
+
+    public CameraMovement CamMove;
+
+    public Transform shotPointLeft;
+    public Transform shotPointRight;
+    public Transform shotPointUp;
+    public Transform shotPointDown;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //localScale = GetComponent<Transform>().localScale;
+    }
+
+    void Update()
+    {
+        //For Movement
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput * speed;
+
+        //ForShoot
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Instantiate(bullet, shotPointLeft.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Instantiate(bullet, shotPointRight.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Instantiate(bullet, shotPointUp.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Instantiate(bullet, shotPointDown.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
 
 
-	public Transform shotPointLeft;
-	public Transform shotPointRight;
-	public Transform shotPointUp;
-	public Transform shotPointDown;
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
 
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody2D>();
-		//localScale = GetComponent<Transform>().localScale;
-	}
-	
-	void Update ()
-	{
-		//For Movement
-		Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-		moveVelocity = moveInput * speed;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "room1")
+        {
+            roomCount = 1;
+            //CamMove.Shake(0.1f,0.1f);
+            CamMove.CameraShift();
+        }
+        if (other.gameObject.tag == "room2")
+        {
+            roomCount = 2;
+            //CamMove.Shake(0.1f, 0.1f);
+            CamMove.CameraShift();
+        }
+        if (other.gameObject.tag == "room3")
+        {
+            roomCount = 3;
+            //CamMove.Shake(0.1f, 0.1f);
+            CamMove.CameraShift();
+        }
+        if (other.gameObject.tag == "room4")
+        {
+            roomCount = 4;
+            // CamMove.Shake(0.1f, 0.1f);
+            CamMove.CameraShift();
+        }
+    }
 
-		//ForShoot
-
-		if (timeBtwShots <= 0)
-		{
-			if (Input.GetKey(KeyCode.LeftArrow))
-			{
-				Instantiate(bullet, shotPointLeft.position, transform.rotation);
-				timeBtwShots = startTimeBtwShots;
-			}
-			else if (Input.GetKey(KeyCode.RightArrow))
-			{
-				Instantiate(bullet, shotPointRight.position, transform.rotation);
-				timeBtwShots = startTimeBtwShots;
-			}
-			else if (Input.GetKey(KeyCode.UpArrow))
-			{
-				Instantiate(bullet, shotPointUp.position, transform.rotation);
-				timeBtwShots = startTimeBtwShots;
-			}
-			else if (Input.GetKey(KeyCode.DownArrow))
-			{
-				Instantiate(bullet, shotPointDown.position, transform.rotation);
-				timeBtwShots = startTimeBtwShots;
-			}
-		}
-		else
-		{
-			timeBtwShots -= Time.deltaTime;
-		}
-	}
-
-
-	private void FixedUpdate()
-	{
-		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
-	}
 
 }
