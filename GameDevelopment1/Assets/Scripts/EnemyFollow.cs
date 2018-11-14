33591bcaf16cf30b667ecb841135aacc73ffc5bf
brtyldz1 <brtyldz32@gmail.com>
2018-11-15 02:38:08 +0300

    Enemy eklemeler

---
+++ GameDevelopment1/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+    public float speed ;
+    public GameObject player;
+    public Vector3 playerOffset;
+    public bool playerTriggered;
+    public GameObject enemy;
+
+    void Start () {
+        playerOffset = new Vector3(Random.Range(0,2),Random.Range(0,2), 0);
+    }
+
+    void Update () {
+
+        if (playerTriggered == true)
+        {
+            enemy.transform.position = Vector2.MoveTowards(enemy.transform.position, player.transform.position - playerOffset, speed*Time.deltaTime);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerTriggered = true;
+        }
+    }
+}
---
+++ GameDevelopment1/Assets/Scripts/EnemyRandomMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRandomMove : MonoBehaviour
+{
+    public bool wallTrigger;
+
+    private float latestDirectionChangeTime;
+    private readonly float directionChangeTime = 4f;
+    private float characterVelocity = 4f;
+    private Vector3 movementDirection;
+    private Vector3 movementPerSecond;
+    private Vector2 center;
+
+    private void Start()
+    {
+        latestDirectionChangeTime = 0f;
+        center = new Vector2(0, 0);
+
+        calcuateNewMovementVector();
+
+    }
+
+    void calcuateNewMovementVector()
+    {
+        movementDirection = new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-15.0f, 15.0f),44.4f).normalized;
+        movementPerSecond = movementDirection * characterVelocity;
+    }
+
+    private void Update()
+    {
+        if (wallTrigger == true)
+        {
+                latestDirectionChangeTime = Time.time;
+                calcuateNewMovementVector();
+        }
+
+        if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        {
+            latestDirectionChangeTime = Time.time;
+            calcuateNewMovementVector();
+        }
+        RandomMove();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            wallTrigger = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            wallTrigger = false;
+        }
+    }
+
+    void RandomMove()
+    {
+        transform.position = new Vector3(transform.position.x + (movementPerSecond.x * Time.deltaTime), transform.position.y + (movementPerSecond.y * Time.deltaTime),44.4f);
+    }
+}
