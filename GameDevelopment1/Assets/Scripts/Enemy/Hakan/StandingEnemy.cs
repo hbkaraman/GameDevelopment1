87dc6085a72f2d5ec92d9ff42e120933b838e73a
hbkaraman <hakanburakkaraman@gmail.com>
2018-12-17 04:58:48 +0300

    Player Anim Added, Standing Shotting Enemy Added
    Ama sadece yukarı ateş ediyor onu halletmek lazım

---
+++ GameDevelopment1/Assets/Scripts/Enemy/Hakan/StandingEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingEnemy : MonoBehaviour {
+
+	private float timeBtwShoots;
+	public float startTimeBtwShoots;
+
+	public GameObject Bullet;
+	public Transform ShottingPoint;
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+		if (timeBtwShoots <= 0)
+		{
+			Instantiate(Bullet, ShottingPoint.position, Quaternion.identity);
+			timeBtwShoots = startTimeBtwShoots;
+		}
+		else
+		{
+			timeBtwShoots -= Time.deltaTime;
+		}
+
+	}
+
+}
---
+++ GameDevelopment1/Assets/Scripts/Enemy/Hakan/StandingEnemyBullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingEnemyBullet : MonoBehaviour {
+
+	public float speed;
+	public float distance;
+	public float lifeTime;
+	public int damage;
+	public LayerMask whatIsSolid;
+
+	public GameObject destroyEffect;
+	private Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke("DestroyProjectile",lifeTime);
+
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = transform.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.forward, distance, whatIsSolid);
+		if (hitInfo.collider != null)
+		{
+			if (hitInfo.collider.CompareTag("Player"))
+			{
+				hitInfo.collider.GetComponent<Player>().TakeDamage(damage);
+			}
+
+			DestroyProjectile();
+		}
+	}
+
+	void DestroyProjectile()
+	{
+		Instantiate(destroyEffect, transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
+}
