30aa7bb84332923491b96d338db666a56fa3e782
brtyldz1 <brtyldz32@gmail.com>
2018-12-23 03:01:35 +0300

    Minimap

---
+++ GameDevelopment1/Assets/MinimapScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapScript : MonoBehaviour {
+
+    public Player player;
+    public Image[] activeImage;
+    public bool roomExit;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (player.roomCount == 1)
+        {
+            activeImage[0].gameObject.SetActive(true);
+        }
+        if (player.roomCount == 2)
+        {
+            activeImage[1].gameObject.SetActive(true);
+        }
+        if (player.roomCount == 3)
+        {
+            activeImage[2].gameObject.SetActive(true);
+        }
+        if (player.roomCount == 4)
+        {
+            activeImage[3].gameObject.SetActive(true);
+        }
+    }
+}
--- GameDevelopment1/Assets/Scripts/RoomScript.cs
+++ GameDevelopment1/Assets/Scripts/RoomScript.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomScript : MonoBehaviour
 {
     public Player pScript;
 
     public bool roomEntered;
+    public bool roomInside;
     public bool roomFinished;
     public int destroyCount;
 
     public DoorScript[] doors;
+    public Image minimapImage;
+    public Image deactiveMinimapImage;
 
     public EnemyScript[] OnDestroyDispatchers;
 
     public UnityEngine.Events.UnityEvent OnAllObjectsDestroyed;
 
+    private void Start()
+    {
+
+    }
 
     void Update()
     {
@@ -33,7 +41,6 @@ public class RoomScript : MonoBehaviour
             {
                 OnDestroyDispatchers[a].gameObject.SetActive(true);
             }
-
         }
 
         if (roomEntered == true && roomFinished == true)
@@ -41,6 +48,17 @@ public class RoomScript : MonoBehaviour
             for (int i = 0; i < doors.Length; i++)
                 doors[i].doorCanOpen = true;
         }
+
+        if (roomInside == true)
+        {
+            minimapImage.gameObject.SetActive(true);
+        }
+        if (roomInside == false&&roomEntered==true)
+        {
+            minimapImage.gameObject.SetActive(false);
+            deactiveMinimapImage.gameObject.SetActive(true);
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -48,6 +66,14 @@ public class RoomScript : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             roomEntered = true;
+            roomInside = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            roomInside = false;
         }
     }
 }
